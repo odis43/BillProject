71d174200128d10431c995a6ef3b798acb7ed780
Oditha <oditha14@gmail.com>
2023-04-06 14:44:36 -0400

    USABills/CADBills change table

--- src/Bill/Bill.jsx
+++ src/Bill/Bill.jsx
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Popup from "reactjs-popup";
 import Bill_Card from "../Bill_Card/Bill_Card";
 import "./Bill.css";
 
 const Bill = () => {
+  const [country, setCountry] = useState(0); // 0 is Canada, 1 is USA
+
+  const canadaBills = ["Bill 1", "Bill 2", "Bill 3", "Bill 4"];
+  const usaBills = ["Bill A", "Bill B", "Bill C", "Bill D"];
+
+  const bills = country === 0 ? canadaBills : usaBills;
+
+  // Split bills into chunks of two
+  const billChunks = bills.reduce(
+    (acc, bill, index) =>
+      index % 2 === 0 ? [...acc, [bill]] : [...acc.slice(0, -1), [...acc.slice(-1)[0], bill]],
+    []
+  );
+
   return (
     <div className="bill">
       <h2>The Bill Project</h2>
       <div className="Buttons">
-        <button>U.S.A</button>
-        <button>Canada</button>
+        <button onClick={() => setCountry(1)}>U.S.A</button>
+        <button onClick={() => setCountry(0)}>Canada</button>
       </div>
       <div>
         <table className="bill-table">
-          <div style={{ alignItems: "center" }}>
-            <h3>Country</h3>
-          </div>
-
+          <thead>
+            <tr>
+              <th colSpan="2">Country</th>
+            </tr>
+          </thead>
           <tbody>
-            <tr>
-              <td>
-                <Bill_Card name="Bill 1" />
-              </td>
-              <td>
-              <Bill_Card name="Bill 2" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-              <Bill_Card name="Bill 3" />
-              </td>
-              <td>
-              <Bill_Card name="Bill 4" />
-              </td>
-            </tr>
+            {billChunks.map((billRow, index) => (
+              <tr key={index}>
+                {billRow.map((billName, index) => (
+                  <td key={index}>
+                    <Bill_Card name={billName} />
+                  </td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
